cdaef0cb6e7599dfe4a6efdc32c967e343d1387d
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-19 02:01:10 +0300

    Stage 1/6: The FASTQ format

--- Read Quality Control/task/control.py
+++ Read Quality Control/task/control.py
@@ -1,1 +1,8 @@
-# write your code here
+# FASTQ_FILE_NAME = "example.fastq"
+
+FASTQ_FILE_NAME = input()
+file = open(FASTQ_FILE_NAME, 'rt')
+lines = file.readlines()
+for line in lines[:4]:
+    print(line, end='')
+file.close()
