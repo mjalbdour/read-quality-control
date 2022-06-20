eeebe2bfa74a47603a15d254105606ee7569c7ba
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-21 02:23:08 +0300

    Stage 6/6: Choose the best

--- Read Quality Control/task/control.py
+++ Read Quality Control/task/control.py
@@ -1,6 +1,87 @@
-# FASTQ_FILE_NAME = "example.fastq"
-# FASTQ_FILE_NAME = "SRR16506265_1.fastq"
+import gzip
 
+
+class FASTQData:
+    def __init__(self, reads=None, total_reads=0, avg_read_len=0, repeats=0, reads_ns=0, avg_gc_content=0, ns_per_seq=0):
+        if reads is None:
+            reads = dict()
+        self.reads = reads
+        self.total_reads = total_reads
+        self.avg_read_len = avg_read_len
+        self.repeats = repeats
+        self.reads_ns = reads_ns
+        self.avg_gc_content = avg_gc_content
+        self.ns_per_seq = ns_per_seq
+
+
+class FASTQDataAnalyzer:
+    @staticmethod
+    def read_archive(archive):
+        file = gzip.open(archive, mode="rt")
+        lines = file.readlines()
+        file.close()
+        return lines
+
+    @staticmethod
+    def analyze_archive(archive):
+        lines = FASTQDataAnalyzer.read_archive(archive)
+        data = FASTQData()
+        sum_avg_ns = 0
+        sum_avg_gc_content = 0
+        for line in lines[1::4]:
+            reading = line.rstrip("\n")
+            k = len(reading)
+            if k not in data.reads:
+                data.reads[k] = 1
+            else:
+                data.reads[k] += 1
+
+            gc_content = 0
+            ns = 0
+            for n in line:
+                if n in {'G', 'C'}:
+                    gc_content += 1
+                elif n == "N":
+                    ns += 1
+            if ns > 0:
+                data.reads_ns += 1
+                sum_avg_ns += ns / k
+
+            sum_avg_gc_content += gc_content / k
+
+        sum_reads = 0
+        for k, v in data.reads.items():
+            data.total_reads += v
+            sum_reads += k * v
+
+        data.avg_read_len = round(sum_reads / data.total_reads)
+        data.avg_gc_content = round(sum_avg_gc_content / data.total_reads * 100, 2)
+        data.ns_per_seq = round(sum_avg_ns / data.total_reads * 100, 2)
+        return data
+
+    @staticmethod
+    def find_best_data(d1, d2, d3):
+        if d1.repeats + d1.reads_ns < d2.repeats + d2.reads_ns:
+            if d1.repeats + d1.reads_ns < d3.repeats + d3.reads_ns:
+                return d1
+            else:
+                return d3
+        else:
+            return d2
+
+    @staticmethod
+    def print_analysis(data):
+        print(f'{MSG_READS} {data.total_reads}')
+        print(f'{MSG_AVERAGE} {data.avg_read_len}')
+
+        print(f'\n{MSG_REPEATS} {data.repeats}')
+        print(f'{MSG_NS_READS} {data.reads_ns}')
+
+        print(f'\n{MSG_GC_CONTENT} {data.avg_gc_content}%')
+        print(f'{MSG_NS} {data.ns_per_seq}%')
+
+
+# MESSAGES
 MSG_READS = "Reads in the file ="
 MSG_LENGTH = "\twith length"
 MSG_AVERAGE = "Reads sequence average length ="
@@ -9,63 +90,15 @@ MSG_GC_CONTENT = "GC content average ="
 MSG_NS_READS = "Reads with Ns ="
 MSG_NS = "Ns per read sequence ="
 
-readings = dict()
-reads_repeats = dict()
-sum_avg_gc_content = 0
-sum_avg_ns = 0
-ns_reads = 0
-repeats = 0
-FASTQ_FILE_NAME = input()
-file = open(FASTQ_FILE_NAME, 'rt')
-lines = file.readlines()
-for line in lines[1::4]:
-    reading = line.rstrip("\n")
-    k = len(reading)
-    if k not in readings:
-        readings[k] = 1
-    else:
-        readings[k] += 1
-    if reading not in reads_repeats:
-        reads_repeats[reading] = 0
-    else:
-        reads_repeats[reading] += 1
-        repeats += 1
+# ARCHIVES
+DATA_1 = "data1.gz"
+DATA_2 = "data2.gz"
+DATA_3 = "data3.gz"
 
-    gc_content = 0
-    ns = 0
-    for n in line:
-        if n in {'G', 'C'}:
-            gc_content += 1
-        elif n == "N":
-            ns += 1
-    if ns > 0:
-        ns_reads += 1
-        sum_avg_ns += ns / k
 
-    sum_avg_gc_content += gc_content / k
+# archives = [DATA_1, DATA_2, DATA_3]
+archives = [input(), input(), input()]
 
-file.close()
+data1, data2, data3 = [FASTQDataAnalyzer.analyze_archive(a) for a in archives]
 
-reads = 0
-sum_reads = 0
-for k, v in readings.items():
-    reads += v
-    sum_reads += k * v
-
-avg_read_len = round(sum_reads / reads)
-avg_gc_content = round(sum_avg_gc_content / reads * 100, 2)
-avg_ns = round(sum_avg_ns / reads * 100, 2)
-
-# print(f'{MSG_READS} {reads}:')
-# for k in sorted(readings.keys()):
-#     print(f'{MSG_LENGTH} {k} = {readings[k]}')
-#
-
-print(f'{MSG_READS} {reads}')
-print(f'{MSG_AVERAGE} {avg_read_len}')
-
-print(f'\n{MSG_REPEATS} {repeats}')
-print(f'{MSG_NS_READS} {ns_reads}')
-
-print(f'\n{MSG_GC_CONTENT} {avg_gc_content}%')
-print(f'{MSG_NS} {avg_ns}%')
+FASTQDataAnalyzer.print_analysis(FASTQDataAnalyzer.find_best_data(data1, data2, data3))
--- Read Quality Control/task/tests.py
+++ Read Quality Control/task/tests.py
@@ -2,21 +2,28 @@ from hstest import *
 import re
 
 
-class NsChecker(StageTest):
+class BestArchive(StageTest):
 
-    def common_test(self, file_name, AMOUNT, AVERAGE, REPEATS, READSN, GC, NSPER):
+    def common_test(self, *files, **point_values):
+        reply = None
         program = TestedProgram()
         program.start()
 
         if not program.is_waiting_input():
             raise WrongAnswer("You program should input the path to the file")
 
-        reply = program.execute(file_name)
+        for data in files:
+            reply = program.execute(data)
 
         # if the reply is empty
         if not reply:
             raise WrongAnswer("You gave an empty answer")
         reply_low = reply.replace(" ", "").lower()
+
+        # dict of correct points
+        point2value_correct = {}
+        for param_name, value in point_values.items():
+            point2value_correct[param_name] = value
 
         # if each point presents only once
         def check_format(line, substring):
@@ -44,30 +51,26 @@ class NsChecker(StageTest):
                 raise WrongAnswer(f"The value of \"{substring}\" is incorrect")
 
         # dict of points for checking + correct values
-        answer_points = {"Reads in the file =": AMOUNT,
-                         "Reads sequence average length =": AVERAGE,
-                         "Repeats =": REPEATS,
-                         "GC content average =": GC,
-                         "Reads with Ns =": READSN,
-                         "Ns per read sequence =": NSPER}
+        substr2point = {"Reads in the file =": 'AMOUNT',
+                        "Reads sequence average length =": 'AVERAGE',
+                        "Repeats =": 'REPEATS',
+                        "GC content average =": 'GC',
+                        "Reads with Ns =": 'READSN',
+                        "Ns per read sequence =": 'NSPER'}
 
         # run checking!
-        for point in answer_points.keys():
-            check_format(reply_low, point)
-        for point, correct_result in answer_points.items():
-            check_number(reply_low, point, correct_result)
+        for substr in substr2point.keys():
+            check_format(reply_low, substr)
+        for substr, point in substr2point.items():
+            check_number(reply_low, substr, point2value_correct[point])
         return CheckResult.correct()
 
     @dynamic_test
     def test1(self):
-        return self.common_test("test/test1.fastq", AMOUNT=4, AVERAGE=101, REPEATS=0,
-                                READSN=4, GC=38.61, NSPER=20.3)
-
-    @dynamic_test
-    def test2(self):
-        return self.common_test("test/test2.fastq", AMOUNT=4, AVERAGE=133, REPEATS=0,
-                                READSN=1, GC=44.49, NSPER=0.17)
+        return self.common_test("test/data1.gz", "test/data2.gz", "test/data3.gz",
+                                AMOUNT=10, AVERAGE=100, REPEATS=0,
+                                READSN=10, GC=41.4, NSPER=22.0)
 
 
 if __name__ == '__main__':
-    NsChecker().run_tests()
+    BestArchive().run_tests()
